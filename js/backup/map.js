1053d3a44ec3d1e01d3d4fbd1c8786d9e50c7b0d
alyhannarivera <pauline.rivera17@gmail.com>
2014-04-07 08:39:07 +0800

    final

--- js/backup/map.js
+++ js/backup/map.js
@@ -12,44 +12,32 @@ var historyInitialized = false;
 
 // Traffic
 
-function addTrafficMarker(lat,lng,desc) {
-//map.removeLayer(circle);
-if(desc == 'M') {
-	var circle = L.circle([lat, lng], 500, {
-		color: 'yellow',
-		fillColor: 'orange',
-		fillOpacity: 1.0
-	}).addTo(map);
-}
-else if(desc == 'H') {
-	var circle = L.circle([lat, lng], 500, {
-		color: 'red',
-		fillColor: '#f03',
-		fillOpacity: 0.5
-	}).addTo(map);
-}
-
-}
-
  var myRoutes = new Array();
  
  $(function() { 
 			$.ajax({
-				url: 'revised.xml',
+				url: 'sample.xml',
 				dataType:'xml',
 				type:'GET',
 				success:function(xml) {
 					var i = 0;
 					$(xml).find('item').each(function() {
 						var title = $(this).find("title").text(); 
-						var northbound = $(this).find("northbound").text();
-						var southbound = $(this).find("southbound").text();
-						var lat = $(this).find("lat").text();
-						var lng = $(this).find("lng").text();
+						var des = $(this).find("description").text();
+						var link = $(this).find("link").text();
+						var $des = $('<div class="linkitem"></div>').html(des);
+						var $link = $('<a></a>').attr('href',link).attr('target','_blank').html(title);
+						var pubDate = new Date($(this).find("pubDate").text()); 
+						var day = pubDate.getDate();
+						var month = pubDate.getMonth() + 1;
+						var year = pubDate.getFullYear();
+						var date = day + '/' + month + '/' + year;
+						var $date = $('<div class="date"></div>').text(date)	
+						var wrapper = "<li class='single-feed'>";
 						
 						var bound = $(this).find("bound").text();
 						
-						myRoute = new Array(title, northbound, southbound, lat,lng);
+						myRoute = new Array(title, des, bound);
 						myRoutes.push(myRoute);
 						i++;
 					})
@@ -81,15 +69,8 @@ var search = new Ractive({
 });
 
 search.addInput = function(id, target) {
-var defaultBounds = new google.maps.LatLngBounds(
-  new google.maps.LatLng(14.740537407131162,120.92651367187499),
-  new google.maps.LatLng(14.37153239246733,121.05148315429686));
-
   var input = document.getElementById(id);
-  
-  var searchBox = new google.maps.places.SearchBox(input, {
-  bounds: defaultBounds
-});
+  var searchBox = new google.maps.places.SearchBox(input);
   var select = function() {
     var places = searchBox.getPlaces();
     var options = {};
@@ -427,42 +408,7 @@ var itinerary = new Ractive({
     },
 	formatTraffic: function(fromRoute, toRoute) {
 	
-		var ctr = -1, i = 0;
-		var trafficStatus;
-		while(i < myRoutes.length && ctr == -1) {
-			
-			ctr = fromRoute.search(myRoutes[i][0]);
-			
-			if(ctr!=-1) break;
-			i++;
-		}
-		
-		
-		if(i < myRoutes.length) {
-		
-			addTrafficMarker(myRoutes[i][3],myRoutes[i][4],myRoutes[i][1]);
-			if(myRoutes[i][1] == 'L') return 'Light Traffic on ' + myRoutes[i][0];
-			else if(myRoutes[i][1] == 'M') return 'Moderate Traffic on ' + myRoutes[i][0];
-			else if(myRoutes[i][1] == 'H') return 'Heavy Traffic on ' + myRoutes[i][0];
-		}
-		
-		else return 'Traffic Unavailable';
-	},
-	formatTrafficStatus: function(direction, traffic, desc, lat, lng) {
-		
-		alert(direction + '----' + traffic);
-		var ctr = direction.search(traffic);
-		
-		if(ctr != 1) {
-			//call function to add marker
-			addTrafficMarker(lat,lng);
-			
-			if(desc == 'L') return 'Light Traffic on ' + traffic;
-			if(desc == 'M') return 'Moderate Traffic on ' + traffic;
-			if(desc == 'H') return 'Heavy Traffic on ' + traffic;
-		}
-		else { return 'PICKABOO';}
-		
+		return 'traffic';
 	}
   }
 });
